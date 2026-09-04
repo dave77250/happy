a75bc94246cc3ac708ab68fb9cb82a0aac577226
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-04 10:15:10 +0200

    Fix (another) compile error

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import { FlexBox, FlexBoxDirection, Text } from "@ui5/webcomponents-react";
 export type HomeProps = {
 };
 
-export function Home(props: HomeProps) {
+export function Home(_props: HomeProps) {
     return (
         <FlexBox direction={FlexBoxDirection.Column} style={{width: '100%' }}>
             <Text>Hello world !</Text>
