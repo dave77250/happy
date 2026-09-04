680948b78873179d5bac3f4cbba22e817d2544c2
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-04 09:55:19 +0200

    Fix compile errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Home } from './components/Home';
-import { IllustratedMessage } from "@ui5/webcomponents-react";
 // import "@ui5/webcomponents-fiori/dist/illustrations/tnt/User2.js";
 
 function App() {
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,9 @@
 import { FlexBox, FlexBoxDirection, Text } from "@ui5/webcomponents-react";
 
-export function Home() {
+export type HomeProps = {
+};
+
+export function Home(props: HomeProps) {
     return 
         <FlexBox direction={FlexBoxDirection.Column} style={{width: '100%' }}>
             <Text>Hello world !</Text>
