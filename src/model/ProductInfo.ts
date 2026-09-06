2dd59f6c5bd19e87369287bb6043d147f701be1f
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-06 17:25:35 +0200

    Final rewrite to (hopefully) make it work

--- src/model/ProductInfo.ts
+++ src/model/ProductInfo.ts
@@ -12,16 +12,24 @@ const offClient = new OpenFoodFacts(window.fetch);
 
 export async function loadOffProductInfo(ean: ProductId): Promise<ProductInfo | undefined> {
     const res = await offClient.getProductV3(ean);
-    const data = res.data;
 
-    if (res.error || data.status !== "success") {
+    if (res.error || !res.data) {
         return undefined;
     }
 
+    const data = res.data;
+
+    if (data.status === "failure") {
+        return undefined;
+    }
+
+    const product = data.product;
+
     const prodInfo: ProductInfo = {
         ean,
-        name: data.product_name_fr || data.product_name,
-        imageUrl: data.image_front_small_url
+        name: product.product_name_fr || product.product_name,
+        imageUrl: product.image_front_small_url
     };
     return prodInfo;
 }
+
