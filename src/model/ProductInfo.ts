0f3d460477ab7b8279652ab9502e4ac9d8744590
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-06 17:20:40 +0200

    Rewrite to resolve access issues

--- src/model/ProductInfo.ts
+++ src/model/ProductInfo.ts
@@ -12,16 +12,16 @@ const offClient = new OpenFoodFacts(window.fetch);
 
 export async function loadOffProductInfo(ean: ProductId): Promise<ProductInfo | undefined> {
     const res = await offClient.getProductV3(ean);
-    if(res.error) {
-        return undefined;
-    } else {
-        const data = res.data;
-        const prodInfo: ProductInfo = {
-            ean,
-            name: data?.product_name_fr || data?.product_name,
-            imageUrl: data?.image_front_small_url
-        }
-        return prodInfo;
-    }
-}
+    const data = res.data;
 
+    if (res.error || data.status !== "success") {
+        return undefined;
+    }
+
+    const prodInfo: ProductInfo = {
+        ean,
+        name: data.product_name_fr || data.product_name,
+        imageUrl: data.image_front_small_url
+    };
+    return prodInfo;
+}
