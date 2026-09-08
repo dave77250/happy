3cddc9f11b863015d8a025990c63646e3513655c
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-08 20:00:50 +0200

    Beginning of dexie integration

--- src/model/ScannedProduct.ts
+++ src/model/ScannedProduct.ts
@@ -1,9 +1,19 @@
 import { ProductInfo } from "./ProductInfo"
+import { Dexie, type EntityTable } from "dexie"
 
 export type ScannedProduct = ProductInfo & {
     scanDate: number,
     isFavorite: boolean
 }
+
+const db = new Dexie("ProductsDatabase") as Dexie & {
+  products: EntityTable<ScannedProduct, "ean">
+}
+
+// Schema declaration:
+db.version(1).stores({
+  products: "ean, name, imageUrl, scanDate, isFavorite",
+})
 
 function createScannedProduct(info: ProductInfo): ScannedProduct {
     return {
