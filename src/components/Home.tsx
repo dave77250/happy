703772ab3359a18c65239f4edac92cfdacb309ff
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-18 20:36:15 +0200

    Tentative de lecture des rappels

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,6 +8,7 @@ export type HomeProps = {
 export function Home(_props: HomeProps) {
     const [productInfo, setProductInfo] = useState("In progress");
     const [imageUrl, setImageUrl] = useState<string|undefined>(undefined);
+    const [recallJson, setRecallJson] = useState("Not yet");
     useEffect(() => {
         loadOffProductInfo("7622210449283").then(pi => {
             if (pi) {
@@ -16,13 +17,16 @@ export function Home(_props: HomeProps) {
             } else {
                 setProductInfo("erreur lors de la lecture du produit");
             }
-        })
+        });
+        fetch("https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/rappelconso-v2-gtin-trie/exports/json/?lang=fr&timezone=Europe%2FParis&where=%28%28%60gtin%60+%3D+3560071492809%29%29")
+        .then(response => response.json()).then(setRecallJson);
     }, []);
     return (
         <FlexBox direction={FlexBoxDirection.Column} style={{width: '100%' }}>
             <Text>Hello world !</Text>
             <Text>{productInfo}</Text>
             {imageUrl ? <img src={imageUrl}/> : <></>}
+            <Text>{recallJson}</Text>
         </FlexBox>
     );
 }
