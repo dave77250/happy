3854d440510db1df7792afd87c95a6235274e2fc
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-18 21:04:39 +0200

    Début de structure lecture rappels

---
+++ src/model/RecallInfo.ts
@@ -0,0 +1,12 @@
+import { ProductId } from "./ProductInfo"
+
+const fetch = window.fetch;
+
+export type RecallInfo = {
+    isRecalled: boolean,
+    recallDetailsUrl: string
+}
+
+export async function getRecallInfo(ean: ProductId) {
+    
+}
