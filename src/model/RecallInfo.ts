a6bd63a09e575d1049df8242d29a8e2be345b7de
dave77250 <63234736+dave77250@users.noreply.github.com>
2026-09-25 19:26:33 +0200

    Begin generating rappel url + debug info

--- src/model/RecallInfo.ts
+++ src/model/RecallInfo.ts
@@ -1,4 +1,5 @@
 import { ProductId } from "./ProductInfo"
+import { debugLog } from "../tools/Debug"
 
 // const fetch = window.fetch;
 
@@ -7,6 +8,19 @@ export type RecallInfo = {
     recallDetailsUrl?: string
 }
 
-export async function getRecallInfo(_ean: ProductId) {
-    
+function buildRappelConsoUrl(ean: ProductId) {
+  const baseUrl = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/rappelconso-v2-gtin-trie/exports/json/";
+  
+  const params = new URLSearchParams({
+    lang: "fr",
+    timezone: "Europe/Paris",
+    where: `((\`gtin\` = ${ean}))`
+  });
+
+  return `${baseUrl}?${params.toString()}`;
+}
+
+export async function getRecallInfo(ean: ProductId) {
+    const url = buildRappelConsoUrl(ean);
+    debugLog("url for " + ean + " is " + url);
 }
